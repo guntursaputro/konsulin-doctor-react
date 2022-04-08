79d087be4e1a6f1769961d9e8de3259319e0d8bb
tpid-nalendro <nalendro@technopartner.co.id>
2022-04-08 14:33:32 +0700

    feat: create add sumary item

--- src/components/SummaryItem.tsx
+++ src/components/SummaryItem.tsx
@@ -1,5 +1,37 @@
-import React from 'react'
+import images from 'assets/images'
+import React, { useState } from 'react'
+import { Button } from './Button/Button'
+import { Input } from './Input'
 
 export const SummaryItem: React.FC = () => {
-  return <div>Summary Item</div>
+  const [form, setForm] = useState('')
+  console.log(form)
+  return (
+    <div>
+      <div className='flex justify-between mt-3 items-center'>
+        <p className='text-xs'>Gatal-Gatal pada permukaan kulit</p>
+        <img src={images.ic_pencil} alt='pencil edit' className='w-5 h-5' />
+      </div>
+      <div className='flex justify-between mt-3 items-center'>
+        <p className='text-xs'>Ruam merah dan perih</p>
+        <img src={images.ic_pencil} alt='pencil edit' className='w-5 h-5' />
+      </div>
+      <div className='flex items-center mt-2'>
+        <Button
+          className='!p-0 !h-fit mr-2'
+          icon={images.ic_plus_yellow}
+          onClick={() => console.log('Submit')}
+        />
+
+        <Input
+          className='w-4/5'
+          placeholder='Tambah Keluhan'
+          inputClass='!p-0 text-xs !border-hidden bg-transparent'
+          name='input'
+          value={form}
+          onChange={(e) => setForm(e.value)}
+        />
+      </div>
+    </div>
+  )
 }
