df8b7080e849e3f4c5bd594ec5c7749356bb58f4
tpid-nalendro <nalendro@technopartner.co.id>
2022-04-08 10:54:33 +0700

    refactor: fix padding bottom

--- src/components/ReviewItem.tsx
+++ src/components/ReviewItem.tsx
@@ -7,7 +7,7 @@ interface ReviewItemProps {
 
 export const ReviewItem: React.FC<ReviewItemProps> = ({ item }) => {
   return (
-    <div className='flex flex-col bg-white px-3 pb-6 pt-3 mt-3 rounded-md drop-shadow'>
+    <div className='flex flex-col bg-white px-3 pb-5 pt-3 mt-3 rounded-md drop-shadow'>
       <div className='flex justify-between'>
         <div className='flex'>{renderStar(item.rating)}</div>
         <p className='text-xxs'>{item.name}</p>
