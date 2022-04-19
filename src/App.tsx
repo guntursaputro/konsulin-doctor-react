a04acbce8dd9321c7e62e24e6607a86a9d6c818f
tpid-nalendro <nalendro@technopartner.co.id>
2022-04-19 11:33:08 +0700

    refactor: add route for consultingdetail and consulting page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
   Route,
   Routes,
 } from 'react-router-dom'
-import { Component, Register, HomeParent } from 'views'
+import { Component, Register, HomeParent, ConsultingDetail } from 'views'
 import { isLogin } from 'utils/auth'
 
 type Props = {
@@ -55,6 +55,12 @@ const App: React.FC<Props> = ({ basename }) => {
               <Route path='/consulting/:type' />
               <Route path='/profile' />
             </Route>
+          </Route>
+          <Route element={<PrivateRoute wrapperContent={false} />}>
+            <Route
+              path='/consulting/detail/:name'
+              element={<ConsultingDetail />}
+            />
           </Route>
           <Route element={<ProtectedRoute />}>
             <Route path='/component' element={<Component />} />
