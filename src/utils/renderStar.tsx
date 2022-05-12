e206bbc8d2b46a00f7b8f0ff94ab98d8e8170ee2
tpid-nalendro <nalendro@technopartner.co.id>
2022-05-12 09:14:49 +0700

    refactor: fix reviewItem component

--- src/utils/renderStar.tsx
+++ src/utils/renderStar.tsx
@@ -4,9 +4,9 @@ export const renderStar = (rate: number) => {
   let tmp = []
   for (let x = 0; x < 5; x++) {
     if (x < rate) {
-      tmp.push(<img src={images.ic_star_fill} alt='' className='w-3 h-3' />)
+      tmp.push(<img src={images.ic_star_fill} alt='' className='w-4 h-4' />)
     } else {
-      tmp.push(<img src='' alt='' className='w-3 h-3' />)
+      tmp.push(<img src='' alt='' />)
     }
   }
   return tmp
