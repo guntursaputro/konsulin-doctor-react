0e1ca0bf663638697a8e20881f73e74319c12459
tpid-nalendro <nalendro@technopartner.co.id>
2022-05-13 16:43:25 +0700

    refactor: add input search component

--- src/views/ConsultingSession/AddRecipe.tsx
+++ src/views/ConsultingSession/AddRecipe.tsx
@@ -1,20 +1,37 @@
 import images from 'assets/images'
-import { Button, Header, MedicineItem } from 'components'
-import React from 'react'
+import { Button, Header, Input, MedicineItem } from 'components'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { MEDICINE_LIST } from 'utils/dumy'
 
 export const AddRecipe: React.FC = () => {
+  const [form, setForm] = useState({
+    name: '',
+  })
   const navigate = useNavigate()
+
+  const handleChangeForm = (e: any) => {
+    setForm({
+      ...form,
+      [e.name]: e.value,
+    })
+  }
+
   return (
     <div className='-mt-5 h-full bg-white -mx-4'>
       <div className='mx-4'>
         <Header onBackClick={() => navigate(-1)} label='Tambah Resep' />
       </div>
-      <div className='bg-white p-4 flex flex-col'>
-        <div className='w-full py-2 px-3 text-xs flex justify-between border rounded-md items-center'>
-          <p>Facial Wash</p>
-          <img src={images.ic_search_loop} alt='' className='w-6 h-6' />
+      <div className='bg-white px-4 flex flex-col'>
+        <div className='w-full py-3 px-3 text-xs flex justify-between border rounded-md items-center'>
+          <Input
+            name='name'
+            value={form?.name}
+            onChange={handleChangeForm}
+            className='text-xxs flex-grow'
+            inputClass='!p-0 text-xs !border-hidden bg-transparent'
+          />
+          <img src={images.ic_search_loop} alt='' className='w-6 h-6 ml-1' />
         </div>
         <img
           src={images.ic_sorting}
--- src/views/ConsultingSession/AddRecipeWithoutReferral.tsx
+++ src/views/ConsultingSession/AddRecipeWithoutReferral.tsx
@@ -1,20 +1,36 @@
 import images from 'assets/images'
-import { Button, Header, MedicineNoReferralItem } from 'components'
-import React from 'react'
+import { Button, Header, Input, MedicineNoReferralItem } from 'components'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { MEDICINE_LIST } from 'utils/dumy'
 
 export const AddRecipeWithoutReferral: React.FC = () => {
+  const [form, setForm] = useState({
+    name: '',
+  })
   const navigate = useNavigate()
+  const handleChangeForm = (e: any) => {
+    setForm({
+      ...form,
+      [e.name]: e.value,
+    })
+  }
+
   return (
     <div className='-mt-5 -mx-4 bg-white -mb-5'>
       <div className='ml-4'>
         <Header onBackClick={() => navigate(-1)} label='Tambah Resep' />
       </div>
       <div className='px-4 bg-white flex flex-col'>
-        <div className='w-full py-2 px-3 text-xs flex justify-between border rounded-md items-center'>
-          <p>Beta</p>
-          <img src={images.ic_search_loop} alt='' className='w-6 h-6' />
+        <div className='w-full py-3 px-3 text-xs flex justify-between border rounded-md items-center'>
+          <Input
+            name='name'
+            value={form?.name}
+            onChange={handleChangeForm}
+            className='text-xxs flex-grow'
+            inputClass='!p-0 text-xs !border-hidden bg-transparent'
+          />
+          <img src={images.ic_search_loop} alt='' className='w-6 h-6 ml-1' />
         </div>
         <img
           src={images.ic_sorting}
